8a665623d21ab39241862acd5208e11058409b49
Shayan Karami <shayankarami.nl@gmail.com>
2023-02-01 14:49:14 +0100

    fix(axios): set axios to read NX_AXIOS_BASE_URL env for non-dev

--- apps/client/utils/axios.ts
+++ apps/client/utils/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export { AxiosError } from 'axios';
 export type { AxiosPromise, AxiosResponse } from 'axios';
 
-const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:3333/api' : 'production_url';
+const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:3333/api' : process.env.NX_AXIOS_BASE_URL;
 
 export const client = axios.create({
   baseURL,
