b2f7b6a0c300f40e40b8bbe4921a2b4475dc64bd
Imam Harir <imam.harir@mister-auto.com>
2023-02-11 15:47:32 +0100

    feat(signUp): implement sign up and account confirmation

--- apps/client/hooks/useSecurity.ts
+++ apps/client/hooks/useSecurity.ts
@@ -3,17 +3,33 @@ import { State } from '../stores/security/types';
 import securityStore, {
   EVENTS, SIGN_IN_REQUEST, SIGN_IN_SUCCESS, SIGN_OUT,
 } from '../stores/security';
-import axiosClient, { SIGN_IN_PATH, USER_INFO_PATH } from '../utils/axios';
+import axiosClient, {
+  SIGN_IN_PATH, SIGN_UP_PATH, USER_INFO_PATH, CONFIRM_ACCOUNT_PATH,
+} from '../utils/axios';
 import useAxios from './useAxios';
+import useTranslation from './useTranslation';
 
 const useSecurity = (): {
   state: State,
   signIn: (object: { username: string, password: string }) => Promise<void>,
+  signUp: (object: { username: string, email: string, password: string }) => Promise<void>,
+  confirmAccount: (object: { username: string, code: string }) => Promise<void>,
   signOut: () => void,
   refreshUserInfo: ()=> Promise<void>,
 } => {
   const [state, setState] = useState<State>(securityStore.state);
   const { data, call } = useAxios('post', SIGN_IN_PATH, { withProgressBar: true });
+  const { trans } = useTranslation();
+  const { call: signUpCall } = useAxios(
+    'post',
+    SIGN_UP_PATH,
+    { withProgressBar: true, customSuccessMessage: trans('signUpSuccessMessage') },
+  );
+  const { call: confirmAccountCall } = useAxios(
+    'post',
+    CONFIRM_ACCOUNT_PATH,
+    { withProgressBar: true, customSuccessMessage: trans('confirmAccountSuccessMessage') },
+  );
 
   function cb() {
     setState(securityStore.state);
@@ -58,6 +74,15 @@ const useSecurity = (): {
       } catch {
         securityStore.emit(SIGN_OUT);
       }
+    },
+    signUp: async (
+      { username, email, password }:
+      { username: string, email: string, password: string },
+    ) => {
+      await signUpCall({ body: { username, email, password } });
+    },
+    confirmAccount: async ({ username, code }: { username: string, code: string }) => {
+      await confirmAccountCall({ body: { username, code } });
     },
     refreshUserInfo: async (): Promise<void> => {
       const response = await axiosClient.get(USER_INFO_PATH);
