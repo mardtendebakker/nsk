b93bd90627eb39b439df2b7237fa8993f1c50019
Shayan Karami <shayankarami.nl@gmail.com>
2023-03-17 11:02:35 +0100

    fix(files): remove duplicated files

--- apps/client/components/memoizedFormInput/Autocomplete.tsx
+++
@@ -1,7 +0,0 @@
-import { memo } from 'react';
-import { Autocomplete } from '@mui/material';
-
-export default memo(
-  Autocomplete,
-  (prevProps, nextProps) => JSON.stringify(prevProps) === JSON.stringify(nextProps),
-);
