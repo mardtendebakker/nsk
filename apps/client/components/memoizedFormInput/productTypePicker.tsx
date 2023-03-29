11cc049182048e56da6ecfa24d1a05136bfa3068
Imam Harir <imam.harir@mister-auto.com>
2023-03-29 23:54:04 +0000

    feat(repairServices): add filter

--- apps/client/components/memoizedFormInput/productTypePicker.tsx
+++ apps/client/components/memoizedFormInput/productTypePicker.tsx
@@ -1,9 +1,10 @@
 import { SxProps } from '@mui/material';
+import { memo } from 'react';
 import useTranslation from '../../hooks/useTranslation';
 import TextField from '../textField';
 import Autocomplete from './autocomplete';
 
-export default function ProductTypePicker(
+function ProductTypePicker(
   {
     disabled, value, sx, fullWidth, label, placeholder, displayFieldset,
   }: {
@@ -54,3 +55,5 @@ ProductTypePicker.defaultProps = {
   placeholder: undefined,
   displayFieldset: true,
 };
+
+export default memo(ProductTypePicker);
--- apps/client/components/stock/repairServices/list/index.tsx
+++ apps/client/components/stock/repairServices/list/index.tsx
@@ -1,4 +1,4 @@
-import { Card } from '@mui/material';
+import { Box, Card } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import _ from 'lodash';
@@ -6,7 +6,8 @@ import { STOCK_REPAIR_SERVICES_PATH } from '../../../../utils/axios';
 import List from './list';
 import useAxios from '../../../../hooks/useAxios';
 import { STOCKS_REPAIR_SERVICES } from '../../../../utils/routes';
-import useForm from '../../../../hooks/useForm';
+import useForm, { FieldPayload } from '../../../../hooks/useForm';
+import Filter from './filter';
 
 function refreshList({
   page,
@@ -19,6 +20,12 @@ function refreshList({
 
   if (page) {
     params.append('page', page.toString());
+  }
+
+  if (formRepresentation.search.value) {
+    const search = formRepresentation.search.value.toString();
+    where.name = { contains: search };
+    params.append('search', search);
   }
 
   call({
@@ -37,24 +44,30 @@ const debouncedRefreshList = _.debounce(refreshList, 500);
 export default function ListContainer() {
   const router = useRouter();
   const [page, setPage] = useState<number>(parseInt(router.query?.page?.toString() || '1', 10));
-  const list = parseInt(router.query?.list?.toString(), 10);
+  const type = parseInt(router.query?.type?.toString(), 10);
+  const taskStatus = parseInt(router.query?.taskStatus?.toString(), 10);
+  const assignedTo = parseInt(router.query?.assignedTo?.toString(), 10);
+  const sortBy = parseInt(router.query?.sortBy?.toString(), 10);
 
-  const { formRepresentation } = useForm({
+  const { formRepresentation, setValue } = useForm({
     search: {
       value: router.query?.search?.toString() || '',
     },
-    createdAt: {
-      value: router.query?.createdAt?.toString() || null,
+    sortBy: {
+      value: Number.isInteger(sortBy) ? sortBy : null,
     },
-    representative: {
-      value: router.query?.representative?.toString() || '',
+    assignedTo: {
+      value: Number.isInteger(assignedTo) ? assignedTo : null,
     },
-    list: {
-      value: Number.isInteger(list) ? list : null,
+    type: {
+      value: Number.isInteger(type) ? type : null,
+    },
+    taskStatus: {
+      value: Number.isInteger(taskStatus) ? taskStatus : null,
     },
   });
 
-  const { data: { data = [], count = 0 } = {}, call } = useAxios(
+  const { data: { data = [], count = 0 } = {}, call, performing } = useAxios(
     'get',
     STOCK_REPAIR_SERVICES_PATH.replace(':id', ''),
     {
@@ -69,10 +82,25 @@ export default function ListContainer() {
       router,
       call,
     });
-  }, [page]);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [
+    page,
+    formRepresentation.search.value,
+    formRepresentation.sortBy.value,
+    formRepresentation.type.value,
+  ]);
 
   return (
     <Card sx={{ overflowX: 'auto', p: '1.5rem' }}>
+      <Filter
+        disabled={performing}
+        formRepresentation={formRepresentation}
+        setValue={(payload: FieldPayload) => {
+          setValue(payload);
+          setPage(1);
+        }}
+      />
+      <Box sx={{ m: '1.5rem' }} />
       <List
         stockRepairServices={data}
         count={Math.floor(count / 10)}
