b4d2bebcb31630fb2a7c67182fc310f331070f55
Shayan Karami <122120643+shayank@users.noreply.github.com>
2023-08-16 22:53:37 +0200

    fix(products): make type_id optional in products update api (#384)

--- apps/server/src/stock/dto/update-body-stock.dto.ts
+++ apps/server/src/stock/dto/update-body-stock.dto.ts
@@ -1,17 +1,9 @@
-import { ApiProperty, ApiPropertyOptional, OmitType, PartialType } from "@nestjs/swagger";
+import { ApiPropertyOptional, OmitType, PartialType } from "@nestjs/swagger";
 import { CreateBodyStockDto } from "./create-body-stock.dto";
 import { ProductOrderUpdateDto } from "./product-order-update.dto";
-import { Transform, Type } from "class-transformer";
-import { IsNumber } from "class-validator";
-import { formDataNumberTransform } from "../../common/transforms/form-date.transform";
+import { Type } from "class-transformer";
 
-export class UpdateBodyStockDto extends PartialType(OmitType(CreateBodyStockDto, ['product_orders', 'type_id'])) {
-  @ApiProperty()
-  @Transform(formDataNumberTransform)
-  @IsNumber()
-  @Type(() => Number)
-  type_id: number;
-
+export class UpdateBodyStockDto extends PartialType(OmitType(CreateBodyStockDto, ['product_orders'])) {
   @ApiPropertyOptional()
   @Type(() => ProductOrderUpdateDto)
   product_orders?: ProductOrderUpdateDto[];
