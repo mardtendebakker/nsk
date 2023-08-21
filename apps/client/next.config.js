4a9bf2fa233a86d974d83bc6cf2be6f59105241e
Shayan Karami <shayankarami.nl@gmail.com>
2023-08-21 14:43:36 +0200

    chore(deps): update nx to 16.7.2

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,10 +1,10 @@
 // @ts-check
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { withNx } = require('@nrwl/next/plugins/with-nx');
+const { withNx } = require('@nx/next/plugins/with-nx');
 
 /**
- * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
+ * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  */
 const nextConfig = {
   nx: {
@@ -13,11 +13,16 @@ const nextConfig = {
     svgr: false,
   },
   env: {
-    MYPTV_API_KEY: 'RVVfMjMyYTdmMDdlZjgwNDllMmI1YTUyNGQ0NWNlMTBhYjA6NjNmOTM4ZjAtNTMzZi00M2EwLWEwNDEtYjhjNmE4N2RiN2Iz',
-    MYPTV_MAP_STYLE_URL: 'https://vectormaps-resources.myptv.com/styles/latest/standard.json',
-    MYPTV_MAP_TILE_URL: 'https://api.myptv.com/maps/v1/vector-tiles/{z}/{x}/{y}',
-    MYPTV_WAY_POINTS_URL: 'https://api.myptv.com/routing/v1/routes?results=POLYLINE&options[trafficMode]=AVERAGE',
-    MYPTV_SEARCH_TEXT_URL: 'https://api.myptv.com/geocoding/v1/locations/by-text?searchText=',
+    MYPTV_API_KEY:
+      'RVVfMjMyYTdmMDdlZjgwNDllMmI1YTUyNGQ0NWNlMTBhYjA6NjNmOTM4ZjAtNTMzZi00M2EwLWEwNDEtYjhjNmE4N2RiN2Iz',
+    MYPTV_MAP_STYLE_URL:
+      'https://vectormaps-resources.myptv.com/styles/latest/standard.json',
+    MYPTV_MAP_TILE_URL:
+      'https://api.myptv.com/maps/v1/vector-tiles/{z}/{x}/{y}',
+    MYPTV_WAY_POINTS_URL:
+      'https://api.myptv.com/routing/v1/routes?results=POLYLINE&options[trafficMode]=AVERAGE',
+    MYPTV_SEARCH_TEXT_URL:
+      'https://api.myptv.com/geocoding/v1/locations/by-text?searchText=',
   },
 };
 
