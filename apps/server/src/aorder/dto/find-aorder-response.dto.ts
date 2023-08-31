f8e849796a072c0d34a7cc8b4e7db350c70214e4
Shayan Karami <shayankarami.nl@gmail.com>
2023-08-31 12:41:09 +0200

    feat(order): add tooltip data to findAll api

--- apps/server/src/aorder/dto/find-aorder-response.dto.ts
+++ apps/server/src/aorder/dto/find-aorder-response.dto.ts
@@ -1,12 +1,62 @@
-import { ApiProperty, PickType } from "@nestjs/swagger";
+import { ApiProperty, ApiPropertyOptional, PickType } from "@nestjs/swagger";
 import { IFindManyRespone } from "../../common/interface/find-many-respone";
 import { AOrderEntity } from "../entities/aorder.entity";
+import { ProductOrderEntity } from "../../stock/entities/product-order.entity";
+import { ProductEntity } from "../../stock/entities/product.entity";
+import { OrderStatusEntity } from "../../order-status/entities/order-status.entity";
+import { CompanyEntity } from "../../company/entities/company.entity";
+
+class OrderStatus extends PickType(OrderStatusEntity, [
+  'id',
+  'name',
+  'color',
+]) {}
+
+class ACompany extends PickType(CompanyEntity, [
+  'id',
+  'name',
+  'street',
+  'city',
+]) {}
+
+class Company extends PickType(CompanyEntity, [
+  'id',
+  'name',
+  'street',
+  'city',
+]) {
+  @ApiPropertyOptional()
+  acompany?: ACompany;
+}
+
+class ProductDto extends PickType(ProductEntity, [
+  'name'
+]) {}
+
+export class ProductOrderDto extends PickType(ProductOrderEntity, [
+  'quantity',
+]) {
+  @ApiPropertyOptional()
+  product?: ProductDto;
+}
 
 class FindAOrderResponeDto extends PickType(AOrderEntity, [
   "id",
   "order_nr",
   "order_date",
-] as const) {}
+] as const) {
+  @ApiPropertyOptional()
+  order_status?: OrderStatus;
+
+  @ApiPropertyOptional()
+  acompany_aorder_supplier_idToacompany?: Company;
+
+  @ApiPropertyOptional()
+  acompany_aorder_customer_idToacompany?: Company;
+
+  @ApiPropertyOptional()
+  product_orders?: ProductOrderDto[];
+}
 
 export class FindAOrdersResponeDto implements IFindManyRespone<FindAOrderResponeDto> {
   @ApiProperty()
