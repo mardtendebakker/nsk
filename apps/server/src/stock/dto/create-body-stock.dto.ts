d57b672e9e392811c28fd1782bf8422c206f2654
Shayan Karami <shayankarami.nl@gmail.com>
2023-09-27 13:41:41 +0200

    feat(order): exclude archived product from add products list in sale order

--- apps/server/src/stock/dto/create-body-stock.dto.ts
+++ apps/server/src/stock/dto/create-body-stock.dto.ts
@@ -1,10 +1,13 @@
-import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
-import { Transform, Type } from "class-transformer";
-import { ProductAttributeDto } from "./product-attribute.dto";
-import { formDataNumberTransform, formDataStringTransform } from "../../common/transforms/form-date.transform";
-import { IsOptional, IsString, IsNumber, IsEnum, IsInt } from "class-validator";
-import { ProductOrderCreateDto } from "./product-order-create.dto";
-import { EntityStatus } from "../../common/types/entity-status.enum";
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Transform, Type } from 'class-transformer';
+import { ProductAttributeDto } from './product-attribute.dto';
+import {
+  formDataNumberTransform,
+  formDataStringTransform,
+} from '../../common/transforms/form-date.transform';
+import { IsOptional, IsString, IsNumber, IsEnum, IsInt } from 'class-validator';
+import { ProductOrderCreateDto } from './product-order-create.dto';
+import { EntityStatus } from '../../common/types/entity-status.enum';
 
 export class CreateBodyStockDto {
   @ApiPropertyOptional()
@@ -18,27 +21,27 @@ export class CreateBodyStockDto {
   @IsOptional()
   @IsString()
   name: string;
-  
+
   @ApiPropertyOptional()
   @Transform(formDataStringTransform)
   @IsOptional()
   @IsString()
   description?: string;
-  
+
   @ApiPropertyOptional()
   @Transform(formDataNumberTransform)
   @IsOptional()
   @IsNumber()
   @Type(() => Number)
   price?: number | null;
-  
+
   @ApiPropertyOptional()
   @Transform(formDataNumberTransform)
   @IsOptional()
   @IsInt()
   @Type(() => Number)
   external_id?: number;
-  
+
   @ApiPropertyOptional()
   @Transform(formDataNumberTransform)
   @IsOptional()
@@ -63,22 +66,21 @@ export class CreateBodyStockDto {
   @ApiPropertyOptional({
     enum: EntityStatus,
     enumName: 'EntityStatus',
-    default: EntityStatus.Active,
   })
   @Transform(formDataNumberTransform)
   @IsOptional()
   @IsEnum(EntityStatus)
   @Type(() => Number)
   entity_status?: EntityStatus;
-  
+
   @ApiPropertyOptional()
   @Type(() => ProductAttributeDto)
   product_attributes?: ProductAttributeDto[];
-  
+
   @ApiPropertyOptional()
   @Type(() => ProductOrderCreateDto)
   product_orders?: ProductOrderCreateDto[];
-  
+
   @ApiPropertyOptional()
   @Transform(formDataNumberTransform)
   @IsOptional()
