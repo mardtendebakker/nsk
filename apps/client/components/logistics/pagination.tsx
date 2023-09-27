c177e51abdcbca2679d0d9bd8a788f346b58d84d
Imam Harir <imam.harir@mister-auto.com>
2023-09-27 22:26:04 +0100

    fix(logistics): logistic bugs

---
+++ apps/client/components/logistics/pagination.tsx
@@ -0,0 +1,38 @@
+import {
+  Box, IconButton, Typography,
+} from '@mui/material';
+import ChevronRight from '@mui/icons-material/ChevronRight';
+import ChevronLeft from '@mui/icons-material/ChevronLeft';
+import { format } from 'date-fns';
+
+export default function Pagination({
+  date,
+  onPrevious,
+  onNext,
+}: {
+  date: Date,
+  onPrevious: () => void,
+  onNext : ()=> void
+}) {
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <IconButton
+        size="small"
+        sx={{ borderRadius: 0, border: '1px solid', mr: '1rem' }}
+        onClick={onPrevious}
+      >
+        <ChevronLeft />
+      </IconButton>
+      <Typography variant="h4">
+        {format(date, 'dd MMMM Y')}
+      </Typography>
+      <IconButton
+        size="small"
+        sx={{ borderRadius: 0, border: '1px solid', ml: '1rem' }}
+        onClick={onNext}
+      >
+        <ChevronRight />
+      </IconButton>
+    </Box>
+  );
+}
