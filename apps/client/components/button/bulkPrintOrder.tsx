6e664da91f88b910c17e8286f04d3a80a340eda4
Shayan Karami <shayankarami.nl@gmail.com>
2024-03-27 10:11:56 +0100

    fix(print): fix wrong import

--- apps/client/components/button/bulkPrintOrder.tsx
+++ apps/client/components/button/bulkPrintOrder.tsx
@@ -4,14 +4,14 @@ import useTranslation from '../../hooks/useTranslation';
 import useAxios from '../../hooks/useAxios';
 import { OrderType } from '../../utils/axios/models/types';
 import {
-  AxiosResponse, BULK_PRINT_PURCHASES_PATH, BULK_PRINT_REPAIRS_PATH, BULK_PRINT_SALES_PATH,
+  AxiosResponse, BULK_PRINT_NORMAL_PURCHASES_PATH, BULK_PRINT_NORMAL_REPAIRS_PATH, BULK_PRINT_NORMAL_SALES_PATH,
 } from '../../utils/axios';
 import { openBlob } from '../../utils/blob';
 
 const AJAX_BULK_PRINT_PATHS = {
-  purchase: BULK_PRINT_PURCHASES_PATH,
-  sales: BULK_PRINT_SALES_PATH,
-  repair: BULK_PRINT_REPAIRS_PATH,
+  purchase: BULK_PRINT_NORMAL_PURCHASES_PATH,
+  sales: BULK_PRINT_NORMAL_SALES_PATH,
+  repair: BULK_PRINT_NORMAL_REPAIRS_PATH,
 };
 
 export default function BulkPrintOrder({
