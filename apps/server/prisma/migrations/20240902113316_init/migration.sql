e78591feebfbf5dcbb370880e65ae55cad7e56a8
Shayan Karami <122120643+shayank@users.noreply.github.com>
2024-09-02 16:08:56 +0200

    refactor(prisma): reset the migrations (#1001)

--- apps/server/prisma/migrations/20240902113316_init/migration.sql
+++ apps/server/prisma/migrations/20240902113316_init/migration.sql
@@ -1,34 +1,4 @@
 -- CreateTable
-CREATE TABLE `acompany` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `partner_id` INTEGER NULL,
-    `name` VARCHAR(255) NOT NULL,
-    `kvk_nr` INTEGER NULL,
-    `representative` VARCHAR(255) NULL,
-    `email` VARCHAR(255) NULL,
-    `phone` VARCHAR(255) NULL,
-    `phone2` VARCHAR(255) NULL,
-    `street` VARCHAR(255) NULL,
-    `street_extra` VARCHAR(255) NULL,
-    `city` VARCHAR(255) NULL,
-    `country` VARCHAR(255) NULL,
-    `state` VARCHAR(255) NULL,
-    `zip` VARCHAR(255) NULL,
-    `street2` VARCHAR(255) NULL,
-    `street_extra2` VARCHAR(255) NULL,
-    `city2` VARCHAR(255) NULL,
-    `country2` VARCHAR(255) NULL,
-    `state2` VARCHAR(255) NULL,
-    `zip2` VARCHAR(255) NULL,
-    `discr` VARCHAR(255) NOT NULL,
-    `is_partner` INTEGER NULL,
-    `external_id` INTEGER NULL,
-
-    INDEX `IDX_ABB0A97D9393F8FE`(`partner_id`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
 CREATE TABLE `afile` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `product_id` INTEGER NULL,
@@ -60,16 +30,12 @@ CREATE TABLE `aorder` (
     `discr` VARCHAR(255) NOT NULL,
     `backingPurchaseOrder_id` INTEGER NULL,
     `external_id` INTEGER NULL,
-    `delivery_type` INTEGER NULL,
-    `delivery_date` DATETIME(0) NULL,
-    `delivery_instructions` LONGTEXT NULL,
 
     UNIQUE INDEX `UNIQ_416119D9360A4EAE`(`order_nr`),
     INDEX `IDX_416119D92ADD6D8C`(`supplier_id`),
     INDEX `IDX_416119D930005451`(`backingPurchaseOrder_id`),
     INDEX `IDX_416119D96BF700BD`(`status_id`),
     INDEX `IDX_416119D99395C3F3`(`customer_id`),
-    INDEX `idx_deliveryDate`(`delivery_date`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -92,7 +58,7 @@ CREATE TABLE `aservice` (
 CREATE TABLE `attribute` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `product_type_filter_id` INTEGER NULL,
-    `attr_code` VARCHAR(11) NOT NULL,
+    `attr_code` VARCHAR(255) NOT NULL,
     `name` VARCHAR(255) NOT NULL,
     `price` INTEGER NULL,
     `type` INTEGER NULL,
@@ -114,6 +80,7 @@ CREATE TABLE `attribute_option` (
     `external_id` INTEGER NULL,
 
     INDEX `IDX_78672EEAB6E62EFA`(`attribute_id`),
+    UNIQUE INDEX `attribute_option_attr_id_name_key`(`attribute_id`, `name`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -132,12 +99,12 @@ CREATE TABLE `fos_user` (
     `roles` LONGTEXT NOT NULL,
     `firstname` VARCHAR(255) NULL,
     `lastname` VARCHAR(255) NULL,
+    `license_plate` VARCHAR(50) NULL,
     `emailCanonical` VARCHAR(255) NOT NULL,
     `partner_id` INTEGER NULL,
 
     UNIQUE INDEX `UNIQ_957A647992FC23A8`(`username_canonical`),
     UNIQUE INDEX `UNIQ_957A6479C05FB297`(`confirmation_token`),
-    INDEX `IDX_957A64799393F8FE`(`partner_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -154,8 +121,9 @@ CREATE TABLE `location` (
 -- CreateTable
 CREATE TABLE `order_status` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `is_sale` BOOLEAN NOT NULL,
     `is_purchase` BOOLEAN NOT NULL,
+    `is_sale` BOOLEAN NOT NULL,
+    `is_repair` BOOLEAN NOT NULL,
     `pindex` INTEGER NULL,
     `name` VARCHAR(255) NOT NULL,
     `color` VARCHAR(7) NULL,
@@ -186,6 +154,7 @@ CREATE TABLE `product` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `type_id` INTEGER NULL,
     `location_id` INTEGER NOT NULL,
+    `location_label_id` INTEGER NULL,
     `status_id` INTEGER NULL,
     `owner_id` INTEGER NULL,
     `sku` VARCHAR(16) NOT NULL,
@@ -194,12 +163,14 @@ CREATE TABLE `product` (
     `price` INTEGER NULL,
     `created_at` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
     `updated_at` DATETIME(0) NOT NULL,
+    `order_updated_at` DATETIME(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
     `external_id` INTEGER NULL,
+    `entity_status` INTEGER NOT NULL DEFAULT 0,
 
     INDEX `IDX_D34A04AD64D218E`(`location_id`),
     INDEX `IDX_D34A04AD6BF700BD`(`status_id`),
-    INDEX `IDX_D34A04AD7E3C61F9`(`owner_id`),
     INDEX `IDX_D34A04ADC54C8C93`(`type_id`),
+    INDEX `product_location_label_id_fkey`(`location_label_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -248,11 +219,12 @@ CREATE TABLE `product_status` (
 CREATE TABLE `product_type` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(255) NOT NULL,
+    `magento_category_id` VARCHAR(255) NULL,
     `pindex` INTEGER NULL,
     `comment` LONGTEXT NULL,
     `is_attribute` BOOLEAN NOT NULL DEFAULT false,
-    `is_public` BOOLEAN NOT NULL DEFAULT true,
     `external_id` INTEGER NULL,
+    `is_public` BOOLEAN NOT NULL DEFAULT true,
 
     UNIQUE INDEX `UNIQ_13675885E237E06`(`name`),
     PRIMARY KEY (`id`)
@@ -294,6 +266,7 @@ CREATE TABLE `task` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(255) NOT NULL,
     `description` LONGTEXT NULL,
+    `pindex` INTEGER NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -308,8 +281,133 @@ CREATE TABLE `user_location` (
     PRIMARY KEY (`user_id`, `location_id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- AddForeignKey
-ALTER TABLE `acompany` ADD CONSTRAINT `FK_ABB0A97D9393F8FE` FOREIGN KEY (`partner_id`) REFERENCES `acompany`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+-- CreateTable
+CREATE TABLE `location_template` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `location_id` INTEGER NOT NULL,
+    `template` VARCHAR(255) NOT NULL,
+    `created_at` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `updated_at` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+
+    INDEX `location_template_location_id_fkey`(`location_id`),
+    UNIQUE INDEX `location_template_location_id_template_key`(`location_id`, `template`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `location_label` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `location_id` INTEGER NOT NULL,
+    `label` VARCHAR(255) NOT NULL,
+    `created_at` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `updated_at` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+
+    INDEX `location_label_location_id_fkey`(`location_id`),
+    UNIQUE INDEX `location_label_location_id_label_key`(`location_id`, `label`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `contact` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `company_id` INTEGER NOT NULL,
+    `name` VARCHAR(255) NULL,
+    `email` VARCHAR(255) NULL,
+    `phone` VARCHAR(255) NULL,
+    `phone2` VARCHAR(255) NULL,
+    `street` VARCHAR(255) NULL,
+    `street_extra` VARCHAR(255) NULL,
+    `city` VARCHAR(255) NULL,
+    `country` VARCHAR(255) NULL,
+    `state` VARCHAR(255) NULL,
+    `zip` VARCHAR(255) NULL,
+    `street2` VARCHAR(255) NULL,
+    `street_extra2` VARCHAR(255) NULL,
+    `city2` VARCHAR(255) NULL,
+    `country2` VARCHAR(255) NULL,
+    `state2` VARCHAR(255) NULL,
+    `zip2` VARCHAR(255) NULL,
+    `is_main` BOOLEAN NULL,
+    `external_id` INTEGER NULL,
+
+    INDEX `contact_company_id_fkey`(`company_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `company` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `partner_id` INTEGER NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `kvk_nr` INTEGER NULL,
+    `is_customer` BOOLEAN NOT NULL DEFAULT false,
+    `is_supplier` BOOLEAN NOT NULL DEFAULT false,
+    `is_partner` BOOLEAN NOT NULL DEFAULT false,
+
+    UNIQUE INDEX `name`(`name`),
+    INDEX `company_partner_id_idx`(`partner_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `payment` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `method` VARCHAR(20) NULL,
+    `transaction_id` VARCHAR(100) NOT NULL,
+    `subscription_id` VARCHAR(100) NULL,
+    `amount` FLOAT NOT NULL,
+    `status` VARCHAR(20) NOT NULL,
+    `created_at` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `updated_at` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `module_payment` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `module_id` INTEGER NULL,
+    `payment_id` INTEGER NULL,
+    `price` FLOAT NOT NULL,
+    `expires_at` DATETIME(0) NOT NULL,
+    `active_at` DATETIME(0) NOT NULL,
+    `created_at` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `updated_at` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+
+    INDEX `fk_module_payment_module_idx`(`module_id`),
+    INDEX `idx_module_payment_module`(`module_id`),
+    INDEX `fk_module_payment_payment_idx`(`payment_id`),
+    INDEX `idx_module_payment_payment`(`payment_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `module` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL,
+    `price` FLOAT NOT NULL,
+    `created_at` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `updated_at` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `config` LONGTEXT NULL,
+
+    UNIQUE INDEX `uniq_module_name`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `delivery` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `order_id` INTEGER NULL,
+    `logistics_id` INTEGER NULL,
+    `type` INTEGER NULL,
+    `date` DATETIME(0) NULL,
+    `instructions` LONGTEXT NULL,
+
+    UNIQUE INDEX `uniq_delivery_order_id`(`order_id`),
+    INDEX `idx_delivery_logistic`(`logistics_id`),
+    INDEX `idx_date`(`date`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
 ALTER TABLE `afile` ADD CONSTRAINT `FK_CFAB40EC4584665A` FOREIGN KEY (`product_id`) REFERENCES `product`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
@@ -321,7 +419,7 @@ ALTER TABLE `afile` ADD CONSTRAINT `FK_CFAB40EC8D9F6D38` FOREIGN KEY (`order_id`
 ALTER TABLE `afile` ADD CONSTRAINT `FK_CFAB40ECC26E160B` FOREIGN KEY (`pickup_id`) REFERENCES `pickup`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE `aorder` ADD CONSTRAINT `FK_416119D92ADD6D8C` FOREIGN KEY (`supplier_id`) REFERENCES `acompany`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `aorder` ADD CONSTRAINT `FK_416119D92ADD6D8C` FOREIGN KEY (`supplier_id`) REFERENCES `contact`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- AddForeignKey
 ALTER TABLE `aorder` ADD CONSTRAINT `FK_416119D930005451` FOREIGN KEY (`backingPurchaseOrder_id`) REFERENCES `aorder`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
@@ -330,7 +428,7 @@ ALTER TABLE `aorder` ADD CONSTRAINT `FK_416119D930005451` FOREIGN KEY (`backingP
 ALTER TABLE `aorder` ADD CONSTRAINT `FK_416119D96BF700BD` FOREIGN KEY (`status_id`) REFERENCES `order_status`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE `aorder` ADD CONSTRAINT `FK_416119D99395C3F3` FOREIGN KEY (`customer_id`) REFERENCES `acompany`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `aorder` ADD CONSTRAINT `FK_416119D99395C3F3` FOREIGN KEY (`customer_id`) REFERENCES `contact`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- AddForeignKey
 ALTER TABLE `aservice` ADD CONSTRAINT `FK_5923AE03256915B` FOREIGN KEY (`relation_id`) REFERENCES `product_order`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
@@ -345,9 +443,6 @@ ALTER TABLE `attribute` ADD CONSTRAINT `FK_FA7AEFFB343A8D62` FOREIGN KEY (`produ
 ALTER TABLE `attribute_option` ADD CONSTRAINT `FK_78672EEAB6E62EFA` FOREIGN KEY (`attribute_id`) REFERENCES `attribute`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE `fos_user` ADD CONSTRAINT `FK_957A64799393F8FE` FOREIGN KEY (`partner_id`) REFERENCES `acompany`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
--- AddForeignKey
 ALTER TABLE `pickup` ADD CONSTRAINT `FK_419E39FD7D418FFA` FOREIGN KEY (`logistics_id`) REFERENCES `fos_user`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- AddForeignKey
@@ -360,10 +455,10 @@ ALTER TABLE `product` ADD CONSTRAINT `FK_D34A04AD64D218E` FOREIGN KEY (`location
 ALTER TABLE `product` ADD CONSTRAINT `FK_D34A04AD6BF700BD` FOREIGN KEY (`status_id`) REFERENCES `product_status`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE `product` ADD CONSTRAINT `FK_D34A04AD7E3C61F9` FOREIGN KEY (`owner_id`) REFERENCES `acompany`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `product` ADD CONSTRAINT `FK_D34A04ADC54C8C93` FOREIGN KEY (`type_id`) REFERENCES `product_type`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE `product` ADD CONSTRAINT `FK_D34A04ADC54C8C93` FOREIGN KEY (`type_id`) REFERENCES `product_type`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `product` ADD CONSTRAINT `product_location_label_id_fkey` FOREIGN KEY (`location_label_id`) REFERENCES `location_label`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- AddForeignKey
 ALTER TABLE `product_attribute` ADD CONSTRAINT `FK_94DA59764584665A` FOREIGN KEY (`product_id`) REFERENCES `product`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
@@ -401,3 +496,26 @@ ALTER TABLE `user_location` ADD CONSTRAINT `FK_BE136DCB64D218E` FOREIGN KEY (`lo
 -- AddForeignKey
 ALTER TABLE `user_location` ADD CONSTRAINT `FK_BE136DCBA76ED395` FOREIGN KEY (`user_id`) REFERENCES `fos_user`(`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
+-- AddForeignKey
+ALTER TABLE `location_template` ADD CONSTRAINT `location_template_location_id_fkey` FOREIGN KEY (`location_id`) REFERENCES `location`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `location_label` ADD CONSTRAINT `location_label_location_id_fkey` FOREIGN KEY (`location_id`) REFERENCES `location`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `contact` ADD CONSTRAINT `contact_company_id_fkey` FOREIGN KEY (`company_id`) REFERENCES `company`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE `company` ADD CONSTRAINT `company_partner_id_fk` FOREIGN KEY (`partner_id`) REFERENCES `company`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE `module_payment` ADD CONSTRAINT `fk_module_payment_payment` FOREIGN KEY (`payment_id`) REFERENCES `payment`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `module_payment` ADD CONSTRAINT `module_payment_module_id_fkey` FOREIGN KEY (`module_id`) REFERENCES `module`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `delivery` ADD CONSTRAINT `fk_delivery_aorder` FOREIGN KEY (`order_id`) REFERENCES `aorder`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE `delivery` ADD CONSTRAINT `fk_delivery_logistics` FOREIGN KEY (`logistics_id`) REFERENCES `fos_user`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
