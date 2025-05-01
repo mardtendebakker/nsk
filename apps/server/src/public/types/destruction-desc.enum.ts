ac824208bf4c90e9ff3cfef4198733a59f2d9949
Shayan Karami <122120643+shayank@users.noreply.github.com>
2025-05-01 13:40:24 +0200

    chore(public): update data destruction text (#1134)

--- apps/server/src/public/types/destruction-desc.enum.ts
+++ apps/server/src/public/types/destruction-desc.enum.ts
@@ -1,9 +1,9 @@
-import { DataDestruction } from './destruction.enum';
+import { DataDestruction } from '../../calendar/pickup/types/destruction.enum';
 
 export const DataDestructionDesc = {
   [DataDestruction.DATADESTRUCTION_NONE]: 'Geen HDD aangeleverd',
   [DataDestruction.DATADESTRUCTION_FORMAT]: 'HDD format',
   [DataDestruction.DATADESTRUCTION_STATEMENT]: 'Statement of destruction',
-  [DataDestruction.DATADESTRUCTION_SHRED]: 'HDD op locatie shredden a €12,50 (extra 0.89ct per KM)',
+  [DataDestruction.DATADESTRUCTION_SHRED]: 'HDD degaussen middels EMP. 100% onbruikbaar maken',
   [DataDestruction.DATADESTRUCTION_KILLDISK]: 'HDD wipe report',
 } as const;
