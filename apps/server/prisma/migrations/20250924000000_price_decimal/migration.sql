c642806ca0286cea7a9e90cdd88f62a77d60a0d6
shayan <shayankarami.nl@gmail.com>
2025-10-03 09:58:41 +0200

    fix(price): back to multiply by 100

---
+++ apps/server/prisma/migrations/20250924000000_price_decimal/migration.sql
@@ -0,0 +1,9 @@
+-- Multiply price values by 100 for all relevant tables
+
+UPDATE aservice SET price = price * 100;
+UPDATE attribute SET price = price * 100;
+UPDATE attribute_option SET price = price * 100;
+UPDATE product SET price = price * 100;
+UPDATE product_order SET price = price * 100;
+UPDATE module_payment SET price = price * 100;
+UPDATE module SET price = price * 100;
